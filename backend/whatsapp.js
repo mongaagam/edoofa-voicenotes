ed5205bbc11e986e3fe95ba37a9adaede3f1267d
Agam Monga <agam@Agams-MacBook-Air.local>
2026-05-27 19:53:55 +0530

    Optimize Puppeteer args for low-memory Render environment

--- backend/whatsapp.js
+++ backend/whatsapp.js
@@ -41,6 +41,9 @@ const client = new Client({
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
       '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
       '--disable-gpu',
       '--disable-web-security'
     ]
